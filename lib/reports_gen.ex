e4975697a9fb419d362e68578510d52f5196b6b5
Lucas Alves <as.lucasalves@gmail.com>
2023-04-22 18:29:56 -0300

    Adding the prices of each report item

--- lib/reports_gen.ex
+++ lib/reports_gen.ex
@@ -1,14 +1,23 @@
 defmodule ReportsGen do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.reduce(initial_report(), fn line, report ->
+      [id, food_name, price] = parse_lie(line)
+      Map.put(report, id,report[id] + price)
+    end)
   end
 
-  defp handle_file({:ok,file_content}) do file_content end
-  defp handle_file({:error,_reason}) do
-    "Error when trying open file or file does not exist"
+  defp parse_lie(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, fn item -> String.to_integer(item) end)
+
+    # |> List.update_at(2,  &String.to_integer/1) -> anonymous function short
   end
 
-
+  def initial_report() do
+    Enum.into(1..30, %{}, fn value -> {Integer.to_string(value), 0} end)
+  end
 end
