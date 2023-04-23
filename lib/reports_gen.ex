f06a6520ec19d9c5e8e1b224105f1666537ae731
Lucas Alves <as.lucasalves@gmail.com>
2023-04-22 21:16:49 -0300

    created function to return foods cost frequence

--- lib/reports_gen.ex
+++ lib/reports_gen.ex
@@ -1,16 +1,37 @@
 defmodule ReportsGen do
   alias ReportsGen.Parser
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(initial_report(), fn [id, _food_name, price], report ->
-      Map.put(report, id, report[id] + price)
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report)
     end)
   end
 
-  def initial_report() do
-    Enum.into(1..30, %{}, fn value -> {Integer.to_string(value), 0} end)
+  def report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, fn value -> {Integer.to_string(value), 0} end)
+
+    %{"users" => users, "foods" => foods }
+  end
+
+  defp sum_values([id, food_name, price], %{"foods"=> foods, "users"=> users} = report) do
+
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users , "foods" => foods }
   end
 
   def fetch_higher_cost(report) do
